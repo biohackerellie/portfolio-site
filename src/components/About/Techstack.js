d390e091e198f524e286fa64b449bf01ac8c451f
ellie <>
2022-11-22 14:19:01 -0700

    modified:   src/components/About/Techstack.js 	modified:   src/components/About/Toolstack.js 	modified:   src/style.css

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,3 +1,4 @@
+import { Tooltip } from "bootstrap";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgMusic } from "react-icons/cg";
@@ -8,44 +9,57 @@ import {
   DiDocker,
   DiWindows,
   DiGit,
+  DiJavascript,
 } from "react-icons/di";
 import {
   SiMicrosoftazure,
   SiAdobecreativecloud,
   SiKubernetes,
+  SiJavascript,
 } from "react-icons/si";
+
+function tips () {
+  ('[data-toggle="tooltip"]').tooltip()
+}
+
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons" onMouseover="displayQuote(Music Production)">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Music Production" >
         <CgMusic />
       </Col>
-      <Col xs={4} md={2} className="tech-icons" onMouseover="displayQuote(Linux)">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Linux">
         <DiLinux/>
       </Col>
-      <Col xs={4} md={2} className="tech-icons" onMouseover="displayQuote(Atlassian Software)">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Atlassian">
         <DiAtlassian />
       </Col>
-      <Col xs={4} md={2} className="tech-icons" onMouseover="displayQuote(Mac OS)">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="MacOS">
         <DiApple />
       </Col>
-      <Col xs={4} md={2} className="tech-icons" onMouseover="displayQuote(Docker)">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Docker">
         <DiDocker />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Adobe Creative Cloud">
         <SiAdobecreativecloud />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Git">
         <DiGit />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Azure">
         <SiMicrosoftazure />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Windows, Server, AD">
         <DiWindows />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Kubernetes">
+        <SiKubernetes />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Javascript">
+        <DiJavascript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Kubernetes">
         <SiKubernetes />
       </Col>
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,3 +1,4 @@
+import { Tooltip } from "bootstrap";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
@@ -6,25 +7,34 @@ import {
   SiPowershell,
   SiPortainer,
   SiAbletonlive,
+  SiNodered,
 } from "react-icons/si";
+
+function tips () {
+  ('[data-toggle="tooltip"]').tooltip()
+}
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Linux">
         <SiLinux />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Visual Studio">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Powershell">
         <SiPowershell />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Portainer">
         <SiPortainer />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="Ableton Live">
         <SiAbletonlive />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" data-toggle="tooltip" data-placement="top" title="NodeJS">
+        <SiNodered />
       </Col>
     </Row>
   );
