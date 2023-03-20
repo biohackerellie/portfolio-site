885c209276bf5be0159dcb87689ef81ae991fd82
biohackerellie <epkerns@gmail.com>
2023-03-20 16:05:53 -0600

    edit project cards

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
+import Card from "react-bootstrap/Card";
 import { BsGithub } from "react-icons/bs";
+import { CgWebsite } from "react-icons/cg";
 
 function ProjectCards(props) {
   return (
@@ -13,16 +13,13 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        {props.ghLink && (
+        <Button  href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
+        )}
+        {props.demoLink && (
           <Button
             variant="primary"
             href={props.demoLink}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+import { Col, Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
-import PS from "./PS.png";
 import docs from "./docs.png";
+import ProjectCard from "./ProjectCards";
+import PS from "./PS.png";
+import python from "./python.png";
+import rds from "./rds.png";
 import site from "./site.png";
-
 
 
 function Projects() {
@@ -23,7 +24,6 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={PS}
-              isBlog={false}
               title="Azure to AD"
               description="I needed a way to convert certain groups from Azure to AD that wasn't offered in our Azure plan, this was what I came up with"
               ghLink="https://github.com/biohackerellie/azure-to-AD-"
@@ -33,7 +33,6 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={docs}
-              isBlog={false}
               title="Documentation"
               description="Documentation site I made for Laurel Public Schools developed with Docusaurus and ReactJS. The site is maintained on prem with scripts to automatically update the hosted docker container on git changes"
               ghLink="https://github.com/Laurel-Public-Schools/links"
@@ -44,11 +43,29 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={site}
-              isBlog={false}
-              title="this site!"
+              title="This website!"
               description="This website is my first personal development project that I've published outside of my day job as a sys admin. Developed with Reactjs, it's been a great learning experience."
               ghLink="https://github.com/biohackerellie/portfolio-site"            
             />
+          </Col>
+        </Row>
+        <Row style ={{ justifyContent: "center", paddingBottom: "50px"}}>
+          <Col md={4} className="project-card">
+          <ProjectCard
+            imgPath={python}
+            title="Google Calendar Emailer"
+            description="A program I wrote in Python to take a list of google calendars and give users an option to send the current weekly events via email"
+            ghLink="https://github.com/biohackerellie/emailer-app"
+            demoLink="https://docs.epklabs.com/Python/CalendarEmail"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={rds}
+              title="Windows RDS using Guacamole"
+              description="A RDS solution using Guacamole as a load-balancer used for Laurel Public Schools students"
+              demoLink="https://docs.epklabs.com/Linux/Docker%20Projects/Windows%20RDS%20with%20Guacamole"
+              />
           </Col>
         </Row>
       </Container>
