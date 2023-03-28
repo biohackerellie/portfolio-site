59b10c808eb9d384dd1bba0aa5d285eb1fc7b839
biohackerellie <epkerns@gmail.com>
2023-03-28 09:48:07 -0600

    fixed resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Container, Row,  } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-//import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import Image from "./res.png";
+import pdf from "./Resume.pdf"
 
 
 
 
-const resume = pdf
+
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <div>
     <Container fluid className=".resume-section">
       <Particle />
-        <Row style={{ justifyContent: "center", position: "relative", paddingTop: "150px" }}>
+        <Row style={{ justifyContent: "center", position: "relative", paddingTop: "100px" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -32,18 +24,10 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download PDF
           </Button>
         </Row>
-      
-
-
-        <Row  className=".resume">  
-          <Document file={resume} className="resume">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}  />
-          </Document>
-          </Row>
-
+				<img src={Image} className="resume"  alt="Resume" />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -52,7 +36,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download PDF
           </Button>
         </Row>
         </Container>
